4624d3e8de9ec87c90188f90ffaa116a401caef4
Abmael <abmael.cont@gmail.com>
2022-09-28 15:19:55 -0300

    feat: entertainment presentation card

---
+++ src/pages/home/EntertainmentPresentationCard.tsx
@@ -0,0 +1,32 @@
+import { EntertainmentPresentation } from "../../proxies/config"
+
+type EntertainmentPresentationCardProps = EntertainmentPresentation;
+
+export const EntertainmentPresentationCard = ({
+    Poster,
+    Title,
+    Year,
+    Type,
+    imdbID,
+}: EntertainmentPresentationCardProps) => {
+    return (
+        <div 
+            data-entertainment-id={imdbID}
+            data-entertainment-type={Type}
+        >
+            <div
+                style={{ pointerEvents: 'none', }}
+            >
+                <img 
+                    src={Poster}
+                    alt={Title}
+                    loading='lazy'
+                />
+            </div>
+            <div>
+                <h3>{Title}</h3>
+                <p>{Year}</p>
+            </div>
+        </div>
+    )
+}
