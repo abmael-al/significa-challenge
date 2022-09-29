db211e3390e0cd357f3f55619b4b13722dcb1075
Abmael <abmael.cont@gmail.com>
2022-09-29 15:08:52 -0300

    feat: use entertainment details custom hook

---
+++ src/hooks/useEntertainmentDetails.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { DetailsRequestConfig, EntertainmentDetails } from "../proxies/config"
+import { getEntertainmentDetails } from "../proxies/getEntertainmentDetails"
+
+export const useEntertainmentDetails = <T extends EntertainmentDetails>(config: DetailsRequestConfig) => {
+    const [details, setDetails] = useState<T | undefined>(undefined);
+    const [isNotFound, setIsNotFound] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        getEntertainmentDetails<T>(config)
+            .then(result => {
+                if(result.Response === 'True') setDetails(result);
+                else if (result.Response === 'False') setIsNotFound(true);
+            })
+            .catch(() => setIsError(true))
+            .finally(() => setIsLoading(false));
+    }, []);
+    
+    return {
+        details,
+        isNotFound,
+        isLoading,
+        isError
+    };
+}
