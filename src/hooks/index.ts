f2d5bf137c489d8edcbbada56c01fac9be1a10fd
Abmael <abmael.cont@gmail.com>
2022-10-04 17:22:47 -0300

    chore: exporting all custom hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,6 @@
+export { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
+export { useEventListener } from './useEventListener';
+export { useEventCallback } from './useEventCallback';
+export { useLocalStorage } from './useLocalStorage'
+
+export { useBookmark } from './useBookmark'
