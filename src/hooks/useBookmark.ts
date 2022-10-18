ff490b23eab9dcd0cec7547afbab91fe0a17d0bf
Abmael <abmael.cont@gmail.com>
2022-10-18 13:50:09 -0300

    refactor: undoing unnecessary useRef

--- src/hooks/useBookmark.ts
+++ src/hooks/useBookmark.ts
@@ -1,26 +1,24 @@
 import { useLocalStorage } from "./";
-import { useRef } from "react";
 
 export function useBookmark(key: string) {
     const [bookmarkItems, setBookmarkItems] = useLocalStorage<string[]>(key, []);
     const bookmark = new Set(bookmarkItems);
-    const features = useRef(
-        {
-            toggle(id: string) {
-                if(bookmark.has(id)) {
-                    bookmark.delete(id)
-                } else {
-                    bookmark.add(id);
-                }
+    const features = {
+        toggle(id: string) {
+            if(bookmark.has(id)) {
+                bookmark.delete(id)
+            } else {
+                bookmark.add(id);
+                
+            }
 
-                setBookmarkItems(Array.from(bookmark));
-            },
-        
-            includes(id: string) {
-                return bookmark.has(id);
-            },
-        }
-    );
+            setBookmarkItems(Array.from(bookmark));
+        },
+    
+        includes(id: string) {
+            return bookmark.has(id);
+        },
+    }
 
-    return features.current;
+    return features;
 }
