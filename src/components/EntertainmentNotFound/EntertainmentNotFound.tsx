72fa90a3823fe2c451b22c08dff2e0c5078c9bb3
Abmael <abmael.cont@gmail.com>
2022-10-25 14:59:37 -0300

    feat: adding a title

--- src/components/EntertainmentNotFound/EntertainmentNotFound.tsx
+++ src/components/EntertainmentNotFound/EntertainmentNotFound.tsx
@@ -13,6 +13,7 @@ export const EntertainmentNotFound = ({ wrapperClass }: EntertainmentNotFoundPro
                 <Error404Icon className='enf__illustration' />
             </div>
             <div className='enf__body'>
+                <h1 className='enf__title'>You weren't supposed to see this</h1>
                 <p className='enf__supporting__text'>Sorry, looks like the movie you're looking for cannot be found.</p>
             </div>
         </div>
