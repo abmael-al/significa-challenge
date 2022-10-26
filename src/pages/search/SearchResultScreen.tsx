be670521cc3e1b49606e1c2bc549478ca944a4bb
Abmael <abmael.cont@gmail.com>
2022-10-26 20:40:50 -0300

    style: making the "empty state" screen responsive

--- src/pages/search/SearchResultScreen.tsx
+++ src/pages/search/SearchResultScreen.tsx
@@ -34,20 +34,20 @@ export const SearchResultScreen = ({ config }: SearchResultScreenProps) => {
 
             {noRequestWasMadeBefore &&
                 <div className="empty__state">
-                    <div>
-                        <figure>
-                            <img 
-                                src="/illustrations/illustration-empty-state.png" 
-                                alt="An illustrantion used to compose an state of empty screen."
-                                width='400px'
-                                height='200px'
-                                loading='lazy'
-                            />
-                        </figure>
-                        <div className="empty__state__body">
-                            <h1 className="empty__state__body__title">Don't know what to search?</h1>
-                            <p className="empty__state__body__copy">Here's an offer you can't refuse</p>
-                        </div>
+                    <figure
+                        className="empty__state__illustration"
+                    >
+                        <img 
+                            src="/illustrations/illustration-empty-state.png" 
+                            alt="An illustrantion used to compose an state of empty screen."
+                            width='400px'
+                            height='200px'
+                            loading='lazy'
+                        />
+                    </figure>
+                    <div className="empty__state__body">
+                        <h1 className="empty__state__body__title">Don't know what to search?</h1>
+                        <p className="empty__state__body__copy">Here's an offer you can't refuse</p>
                     </div>
                 </div>
             }
